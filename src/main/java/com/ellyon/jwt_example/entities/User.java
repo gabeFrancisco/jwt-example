fc3454d1b9161e8c26b4a48607647518c11004e4
gabeFrancisco <gabegit2@gmail.com>
2025-10-03 22:30:55 -0300

    project setup and first entities

---
+++ src/main/java/com/ellyon/jwt_example/entities/User.java
@@ -0,0 +1,23 @@
+package com.ellyon.jwt_example.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    private String username;
+    private String password;
+
+    private Set<Role> roles;
+}
