912f92448d8ef92dabfc588184d3fb4f520ff5fb
gabeFrancisco <gabegit2@gmail.com>
2025-10-11 21:30:43 -0300

    created JwtService class

---
+++ src/main/java/com/ellyon/jwt_example/controllers/JwtService.java
@@ -0,0 +1,10 @@
+package com.ellyon.jwt_example.controllers;
+
+import org.springframework.stereotype.Service;
+import org.springframework.security.oauth2.j;;;
+
+@Service
+public class JwtService {
+    private final NimbusJwtEncoder encoder;
+    
+}
